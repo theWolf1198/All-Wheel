34a98bbf1556d0ff09957e854338913fffcddfd2
theWolf1198 <kavel.barua@gmail.com>
2018-06-11 01:14:05 +0530

    First Commit

---
+++ app/src/main/java/com/example/kavel/allwheel_2/MainActivity.java
@@ -0,0 +1,136 @@
+package com.example.kavel.allwheel_2;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth UserAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private static final int RC_SIGNIN = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        UserAuth = FirebaseAuth.getInstance();
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.PhoneBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build());
+
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setIsSmartLockEnabled(false)
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGNIN);
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user!=null){
+
+                    Toast.makeText(MainActivity.this, "Your're now signed in. ", Toast.LENGTH_SHORT).show();
+
+                } else {
+
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setIsSmartLockEnabled(false)
+                                    .setAvailableProviders(Arrays.asList(
+                                            new AuthUI.IdpConfig.PhoneBuilder().build(),
+                                            new AuthUI.IdpConfig.GoogleBuilder().build()))
+                                    .build(),
+                            RC_SIGNIN);
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGNIN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                String name = user.getDisplayName();
+                Toast.makeText(MainActivity.this, "Signed in successfully. Mr. " + name, Toast.LENGTH_SHORT).show();
+                // ...
+            } else {
+
+                if (response == null) {
+
+                    finish();
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.signout) {
+
+            AuthUI.getInstance()
+                    .signOut(this)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        public void onComplete(@NonNull Task<Void> task) {
+
+                            Toast.makeText(MainActivity.this, "Signed out successfully. ", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+            return true;
+        }
+
+        Toast.makeText(MainActivity.this, "Signed out button didn't work", Toast.LENGTH_SHORT).show();
+        return false;
+
+
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        UserAuth.removeAuthStateListener(mAuthStateListener);
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        UserAuth.addAuthStateListener(mAuthStateListener);
+    }
+}
+
